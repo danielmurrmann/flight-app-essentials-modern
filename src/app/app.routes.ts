1f2bf8a79d7bffcd7ed90e5602740a6b90319411
Daniel Murrmann <daniel.murrmann@fancy-development.net>
2026-01-29 10:49:54 +0100

    801 – Create a HomeView Component and set up Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeView } from './home-view/home-view';
+import { FlightSearchView } from './flight-search-view/flight-search-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+},{
+    path: 'home',
+    component: HomeView
+}, {
+    path: 'flight-search',
+    component: FlightSearchView
+}, {
+    path: '**',
+    redirectTo: '/home'
+}];
